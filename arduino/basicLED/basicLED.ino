56e42f1f6d56775b153c0c575c1934f852966ae9
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-02-06 12:33:35 -0500

    week3 class

---
+++ arduino/basicLED/basicLED.ino
@@ -0,0 +1,12 @@
+
+#define led 13
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+}
